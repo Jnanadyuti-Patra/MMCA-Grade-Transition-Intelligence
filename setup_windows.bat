fa78e8d1e5de0cfc26e76b600dbacac36c7c7e04
Jnanadyuti Patra <jnanadyuti4@gmail.com>
2026-07-23 12:24:52 +0800

    Delete setup_windows.bat

--- setup_windows.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-py -3.12 -m venv .venv
-call .venv\Scripts\activate.bat
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
-echo.
-echo Setup complete. Run run_local.bat.
-pause
